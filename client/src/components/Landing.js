2601861069107c35a0e85d29213c664cb6d356ee
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-10 20:19:45 -0400

    refactor styled components and add auth redirect

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,65 +1,55 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import bg from '../images/background.jpg';
+import 
+{
+  A,
+  DIV1,
+  IMG,
+  H1,
+  P
+} from './styledComponents/Landing';
 
-const textStyles = css`
-  color: white;
-  text-shadow: 1px 1px #000;
-`
+class Landing extends Component {
 
-const H1 = styled.h1`
-  ${textStyles}
-  font-size: 5rem;
-`
-
-const DIV1 = styled.div`
-  padding-top: 30vh;
-
-  @media (max-width: 320px) {
-    padding-top: 20vh;
+  renderContent() {
+    return (
+      <div>
+        <A href="/auth/google">Sign In</A>
+        <DIV1 className="text-center">
+          <IMG src={bg} alt=""></IMG>
+          <div className="container">
+            <H1>WELCOME TO CARRY ME</H1>
+            <P>The ultimate network for gamers looking to help others accomplish goals while earning some money</P>
+          </div>
+        </DIV1>
+      </div>
+    );
   }
-`
 
-const IMG = styled.img`
-  position: fixed; 
-  top: 0; 
-  left: 0; 
-  z-index: -1;
-  max-width: 100%;
-  min-height: 100%;
-  height: auto;
-`
-
-const P = styled.p`
-  ${textStyles}
-  font-size: 2.5rem;
-`
-
-const A = styled.a`
-  ${textStyles}
-  float: right;
-  padding: 10px 20px 0 0;
-  font-size: 18px;
-
-  &:hover {
-    ${textStyles}
-    font-size: 19px;
+  checkAuth() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return this.renderContent();
+      default:
+        return <Redirect to='/dashboard'/>;
+    }
   }
-`
 
-const Landing = () => {
-  return (
-    <div>
-      <A href="/auth/google">Sign In</A>
-      <DIV1 className="text-center">
-        <IMG src={bg} alt=""></IMG>
-        <div className="container">
-          <H1>WELCOME TO CARRY ME</H1>
-          <P>The ultimate network for gamers looking to help others accomplish goals while earning some money</P>
-        </div>
-      </DIV1>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        {this.checkAuth()}
+      </div>
+    );
+  }
 }
 
-export default Landing;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Landing);
