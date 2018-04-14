f33c4aee7ff84819a169e5171512bab8bcce8775
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-13 22:53:36 -0400

    add submit user settings action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,3 +6,9 @@ export const fetchUser = () => async dispatch => {
 
   dispatch({ type: FETCH_USER, payload: res.data });
 }
+
+export const submitUserSettings = (values) => async dispatch => {
+  const res = await axios.put('/api/settings', values);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+}
