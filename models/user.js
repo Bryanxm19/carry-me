1e72c9240741cbffe45fac1ff4f3df2881f52d14
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-13 22:54:29 -0400

    add unique validation for username and email

--- models/user.js
+++ models/user.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new Schema({
   googleID: String,
-  email: String,
-  username: String
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+   }
 },
 {
   timestamps: true
 });
 
+userSchema.plugin(uniqueValidator);
 mongoose.model('users', userSchema);
