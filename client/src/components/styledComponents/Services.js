4e6d30dc767e7195a046d59e00cdcc33ed961d8b
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-17 16:51:53 -0400

    create new styled components for services

---
+++ client/src/components/styledComponents/Services.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const DIV = styled.div`
+  @media (min-width: 992px) {
+    width: 55%;
+  }
+`
+
+export const LABELDIV = styled.div`
+  @media (max-width: 767px) {
+    text-align: center !important;
+  }
+`
