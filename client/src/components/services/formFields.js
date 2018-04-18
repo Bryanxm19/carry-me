50ba56aa21fdc41f3b95ed9a0d3f5dd5c5b04162
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-17 21:24:40 -0400

    fix field placeholders

--- client/src/components/services/formFields.js
+++ client/src/components/services/formFields.js
@@ -1,7 +1,7 @@
 export default [
   { label: 'Type', name: 'type', type: 'select', placeholder: '', options: ['carries', 'goals'] },
-  { label: 'Title', name: 'title', type: 'text', placeholder: 'ex: Win a Game of Fortnite' },
-  { label: 'Description', name: 'description', placeholder: { "carries": "ex: I will get you a win in Fornite", "goals": "ex: I have never won a game of Fortnite and need some help" }, type: 'textarea' },
+  { label: 'Title', name: 'title', type: 'text', placeholder: 'Example: Win a Game of Fortnite' },
+  { label: 'Description', name: 'description', placeholder: { "carries": "Example: I will get you a win in Fortnite", "goals": "Example: I have never won a game of Fortnite and need some help" }, type: 'textarea' },
   { label: 'Game', name: 'game', placeholder: '', type: 'text' },
   { label: 'Platform', name: 'platform', placeholder: '', type: 'select' },
   { label: 'Price', name: 'price', placeholder: '', type: 'number' }
