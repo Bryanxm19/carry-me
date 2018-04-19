db6c74675192dc257f66a8798de771057706ce9d
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-19 17:01:21 -0400

    remove types from form fields

--- client/src/components/services/formFields.js
+++ client/src/components/services/formFields.js
@@ -1,8 +1,8 @@
 export default [
-  { label: 'Type', name: 'type', type: 'select', placeholder: '', options: ['carries', 'goals'] },
-  { label: 'Title', name: 'title', type: 'text', placeholder: 'Example: Win a Game of Fortnite' },
-  { label: 'Description', name: 'description', placeholder: { "carries": "Example: I will get you a win in Fortnite", "goals": "Example: I have never won a game of Fortnite and need some help" }, type: 'textarea' },
-  { label: 'Game', name: 'game', placeholder: '', type: 'text' },
-  { label: 'Platform', name: 'platform', placeholder: '', type: 'select' },
-  { label: 'Price', name: 'price', placeholder: '', type: 'number' }
+  { label: 'Type', name: 'type', placeholder: '', options: ['carries', 'goals'] },
+  { label: 'Title', name: 'title', placeholder: 'Example: Win a Game of Fortnite' },
+  { label: 'Description', name: 'description', placeholder: { "carries": "Example: I will get you a win in Fortnite", "goals": "Example: I have never won a game of Fortnite and need some help" } },
+  { label: 'Game', name: 'game', placeholder: '' },
+  { label: 'Platform', name: 'platform', placeholder: '' },
+  { label: 'Price', name: 'price', placeholder: '' }
 ];
