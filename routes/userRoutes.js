75730e7a114a4b643195486800b1626c75305499
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-18 23:25:08 -0400

    handle error for games lookup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -39,7 +39,7 @@ module.exports = app => {
     }).then(response => {
       res.status(200).send(response.body)
     }).catch(error => {
-        throw error;
+        res.status(500).send('Unknown Server Error')
     });
   });
 };
