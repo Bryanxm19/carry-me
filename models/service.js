f77e7062024a5146095067b5c8624ecaa826f4c0
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-21 01:24:30 -0400

    create service model

---
+++ models/service.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const serviceSchema = new Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  game: {
+    type: String,
+    required: true
+  },
+  platform: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User' 
+  }
+},
+{
+  timestamps: true
+});
+
+mongoose.model('services', serviceSchema);
