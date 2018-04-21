329562055ee5d3304004760fa5b13582aa9ce5d9
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-21 01:24:15 -0400

    create services routes

---
+++ routes/serviceRoutes.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const igdb = require('igdb-api-node').default;
+const keys = require('../config/keys');
+const requireLogin = require('../middlewares/requireLogin');
+
+const User = mongoose.model('users');
+const Service = mongoose.model('services');
+
+module.exports = app => {
+  app.post('/api/lookup_games', requireLogin, (req, res) => {
+    const client = igdb(keys.igdbKey);
+    client.games({
+        filters: {
+          'release_dates.platform-eq': 48,
+          'release_dates.platform-eq': 49,
+          'release_dates.platform-eq': 6
+        },
+        fields: '*', // Return all fields
+        limit: 5, // Limit to 5 results
+        order: 'popularity:desc',
+        search: req.body.query
+    }).then(response => {
+      res.status(200).send(response.body)
+    }).catch(error => {
+        console.log(error)
+        res.status(500).send('Unknown Server Error')
+    });
+  });
+
+  app.post('/api/services', requireLogin, async (req, res) => {
+    const params = req.body
+    const user = await User.findById(req.user._id)
+    params.creator = user
+    var service = new Service(params);
+    service.save(err => {
+      if (err) {
+        console.log(err)
+        res.status(500).send('Unknown Server Error')
+      } else {
+        if (params['type'] === 'carries') {
+          user.carries.push(service)
+        } else {
+          user.goals.push(service)
+        }
+        user.save()
+          .then(user => {
+            res.status(200).send(user)
+          })
+          .catch(err => {
+            console.log(err)
+            res.status(500).send('Unknown Server Error')
+          })
+      }
+    })
+  });
+}
