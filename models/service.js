ff6c29d2943a96fc8ff2ef8c87c98ba3a4e12354
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-21 14:58:34 -0400

    add image url for game pic

--- models/service.js
+++ models/service.js
@@ -22,6 +22,10 @@ const serviceSchema = new Schema({
     type: String,
     required: true
   },
+  imageURL: {
+    type: String,
+    required: true
+  },
   price: {
     type: Number,
     required: true
