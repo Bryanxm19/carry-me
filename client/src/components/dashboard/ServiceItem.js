745925c039b6b7aac1786956a61d6bf547bb7a39
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-26 15:34:32 -0400

    add service item component

---
+++ client/src/components/dashboard/ServiceItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { ITEM } from '../styledComponents/Services';
+
+class ServiceItem extends Component {
+
+  state = { hovered: false }
+
+  handleMouseHover() {
+    this.setState({ hovered: !this.state.hovered })
+  }
+
+  renderContent() {
+    if (this.state.hovered) {
+      const { service } = this.props;
+      return (
+        <div className="text-center service-item-overlay" style={{ position: 'absolute', top: 0, bottom: '20px', left: '15px', right: '15px', borderRadius: '4px' }}>
+          <h3 style={{ color: 'white', opacity: 1, marginTop: '40px' }}>{service.title} for ${service.price}</h3>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    const { service } = this.props;
+    return (
+      <div className="col-xs-4 col-md-6" style={{ marginTop: '10px' }}>
+        <ITEM className="thumbnail" style={{ backgroundImage: `url(${service.imageURL})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', overflow: 'hidden', height: '150px' }} onMouseEnter={this.handleMouseHover.bind(this)} onMouseLeave={this.handleMouseHover.bind(this)}>
+          {this.renderContent()}
+        </ITEM>
+      </div>
+    )
+  }
+};
+
+export default ServiceItem;
