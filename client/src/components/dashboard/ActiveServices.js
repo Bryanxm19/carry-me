8abce8763cc7491391976eedd382a98c3e8f6f01
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-26 15:29:04 -0400

    fix styling

--- client/src/components/dashboard/ActiveServices.js
+++ client/src/components/dashboard/ActiveServices.js
@@ -7,8 +7,8 @@ import ServiceList from './ServiceList';
 class ActiveServices extends Component {
   render() {
     return (
-      <div className="col-md-5" style={{ backgroundColor: 'white', borderRadius: '.25rem' }}>
-        <div className="row" style={{ borderBottom: '2px solid #314459', marginBottom: '10px' }}>
+      <div className="col-md-5" style={{ backgroundColor: 'white', borderRadius: '.25rem', marginBottom: '10px' }}>
+        <div className="row" style={{ borderBottom: '2px solid #314459' }}>
           <div className="col-xs-8">
             <h1>Active {this.props.type}</h1>
           </div>
