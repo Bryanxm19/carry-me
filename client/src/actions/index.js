bfabbee0ae2067db3ea595c87e095bce74d3d304
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-26 15:27:20 -0400

    add action for getting the user's goals

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
-import { FETCH_USER, FETCH_ERRORS, CLEAR_ERRORS } from './types';
+import { 
+  FETCH_USER,
+  FETCH_USER_GOALS,
+  FETCH_ERRORS,
+  CLEAR_ERRORS 
+} from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
 
   dispatch({ type: FETCH_USER, payload: res.data });
+}
+
+export const fetchUserGoals = (limit = 10) => async dispatch => {
+  const res = await axios.get('/api/goals?limit={limit}');
+
+  dispatch({ type: FETCH_USER_GOALS, payload: res.data })
 }
 
 export const submitUserSettings = (values, history) => dispatch => {
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const FETCH_USER = 'fetch_user';
+export const FETCH_USER_GOALS = 'fetch_user_goals';
 export const FETCH_ERRORS = 'fetch_errors';
 export const CLEAR_ERRORS = 'clear_errors';
