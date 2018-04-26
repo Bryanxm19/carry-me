3101f5fe9171f1f2b7e3368dfd97b5a245f3cd6c
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-26 15:33:07 -0400

    add goals reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import goalsReducer from './goalsReducer';
 import errorReducer from './errorReducer';
 
 export default combineReducers({
   auth: authReducer,
+  goals: goalsReducer,
   form: reduxForm,
   errors: errorReducer
 });
