73296f874745df54ac87e19d9d3e963e87f88a8b
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-26 15:29:26 -0400

    get goals from props

--- client/src/components/dashboard/CarriesContainer.js
+++ client/src/components/dashboard/CarriesContainer.js
@@ -3,21 +3,13 @@ import React, { Component } from 'react';
 import ActiveServices from './ActiveServices';
 
 class CarriesContainer extends Component {
-
-  find(carryType) {
-    const { carries, goals } = this.props.auth;
-    if (carryType === "carries") {
-      return carries
-    }
-    return goals
-  }
-
   render() {
+    const { goals } = this.props
     return (
       <div className="row">
-        <ActiveServices type="Carries" services={this.find('carries')} />
+        <ActiveServices type="Carries" services={[]} />
         <div className="col-md-2"></div>
-        <ActiveServices type="Goals" services={this.find('goals')} />
+        <ActiveServices type="Goals" services={goals} />
       </div>
     );
   }
