2a64ef0a4a6153c69e2d4579f2513357903eac04
Bryan Mateer <bryanxmateer@gmail.com>
2018-04-26 15:32:21 -0400

    specify platforms in game label

--- client/src/components/services/formFields.js
+++ client/src/components/services/formFields.js
@@ -2,7 +2,7 @@ export default [
   { label: 'Type', name: 'type', placeholder: '', options: ['carries', 'goals'] },
   { label: 'Title', name: 'title', placeholder: 'Example: Win a Game of Fortnite' },
   { label: 'Description', name: 'description', placeholder: { "carries": "Example: I will get you a win in Fortnite", "goals": "Example: I have never won a game of Fortnite and need some help" } },
-  { label: 'Game', name: 'game', placeholder: '' },
+  { label: 'Game (PC/PS4/XONE)', name: 'game', placeholder: '' },
   { label: 'Platform', name: 'platform', placeholder: '' },
   { label: '', name: 'imageURL', placeholder: '' },
   { label: 'Price', name: 'price', placeholder: '' }
