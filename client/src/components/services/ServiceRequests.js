90d5d3f31ac16860efea5dbf043d4258685e194a
Bryan Mateer <bryanxmateer@gmail.com>
2018-05-10 19:43:30 -0400

    switch to class based component

--- client/src/components/services/ServiceRequests.js
+++ client/src/components/services/ServiceRequests.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ServiceRequests = (props) => {
-  return (
-    <div className="col-sm-offset-1 col-lg-7 col-md-6 col-sm-5 col-xs-12" style={{ backgroundColor: 'white', borderRadius: '.25rem' }}>
-    </div>
-  );
-};
+class ServiceRequests extends Component {
+  render() {
+    return (
+      <div className="col-sm-offset-1 col-lg-7 col-md-6 col-sm-5 col-xs-12" style={{ backgroundColor: 'white', borderRadius: '.25rem' }}>
+        <div className="row" style={{ borderBottom: '2px solid #314459' }}>
+          <div className="col-xs-12 text-center">
+            <h1 style={{ marginTop: '10px' }}>Requests</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ServiceRequests;
