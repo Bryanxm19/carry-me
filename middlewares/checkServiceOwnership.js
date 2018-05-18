79ff0b6152482f5a4dd12a5aebcf92941aceb2a3
Bryan Mateer <bryanxmateer@gmail.com>
2018-05-17 22:02:27 -0400

    fix method to check ownership

--- middlewares/checkServiceOwnership.js
+++ middlewares/checkServiceOwnership.js
@@ -5,9 +5,14 @@ const User = mongoose.model('users')
 module.exports = async (req, res, next) => {
   const request = await Request.findById(req.params.id)
   const user = await User.findById(req.user._id)
-  console.log(user.carries.includes(request.service.toString()))
-  console.log(user.goals.includes(request.service.toString()))
-  if (!user.carries.includes(request.service.toString()) && !user.goals.includes(request.service.toString())) {
+
+  const ownCarry = user.carries.some(carry => {
+      return request.service.equals(carry)
+  })
+  const ownGoal = user.goals.some(goal => {
+      return request.service.equals(goal)
+  })
+  if (!(ownCarry || ownGoal)) {
     return res.status(401).send({ error: "You don't have permission to do that" })
   }
 
