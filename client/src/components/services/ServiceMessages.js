d0bb9219002cd3004ffe0b9131f6b0c7ed6dfe9f
Bryan Mateer <bryanxmateer@gmail.com>
2018-06-12 00:35:03 -0400

    set up sockets

--- client/src/components/services/ServiceMessages.js
+++ client/src/components/services/ServiceMessages.js
@@ -12,24 +12,27 @@ class ServiceMessages extends Component {
     super(props);
 
     this.state = {
-        message: ''
+      message: '',
+      messages: this.props.messages
     };
-    this.socket = io('https://carry-me-dev.herokuapp.com');
+    this.socket = io('http://localhost:5000/');
 
-    this.socket.on('RECEIVE_MESSAGE', function(data){
+    this.socket.on('RECEIVE_MESSAGE', (data) => {
       sendMessage(data)
     });
 
     const sendMessage = data => {
-      this.props.sendMessage(data.requestId, { body: data.body })
+      this.props.sendMessage(data.request, data)
     }
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchMessages(this.props.request._id)
+    let listContainer = document.getElementById("message-list");
+    listContainer.scrollTop = listContainer.scrollHeight;
   }
 
-  componentDidMount() {
+  componentDidUpdate() {
     let listContainer = document.getElementById("message-list");
     listContainer.scrollTop = listContainer.scrollHeight;
   }
@@ -40,25 +43,35 @@ class ServiceMessages extends Component {
 
   sendMessage(e) {
     e.preventDefault();
-    this.socket.emit('SEND_MESSAGE', {
+    const { request, auth } = this.props
+    const message = {
       body: this.state.message,
-      requestId: this.props.request._id
-    });
-    this.setState({message: ''});
+      request: request._id,
+      user: auth
+    }
+    this.socket.emit('SEND_MESSAGE', message);
+    this.setState({ message: '', messages: this.props.messages.push(message) });
   }
 
   renderMessages() {
     const { messages, auth } = this.props;
-    console.log(messages)
-    _.forEach(messages.messages, message => {
-      <ServiceMessage message={message} position={ message.user === auth._id ? "right" : "left" } />
-    })
+    if (messages.length) {
+      return _.map(messages, (message, i) => {
+        return <ServiceMessage key={i} message={message} position={ message.user._id === auth._id ? "right" : "left" } />
+      })
+    } else {
+      return(
+        <div className="col-xs-12 text-left">
+          <p className="text-center">There are no messages currently. Start this conversation and plan out when this carry will take place before sending payments.</p>
+        </div>
+      )
+    }
   }
 
   render() {
     return(
       <div className="row">
-        <div id="message-list" style={{ overflowY: 'scroll', maxHeight: '300px', borderBottom: '2px solid #314459' }}>
+        <div id="message-list" style={{ overflowY: 'scroll', maxHeight: '300px', borderBottom: '2px solid #314459', paddingBottom: '10px' }}>
           {this.props.messages && this.renderMessages()}
         </div>
         <div className="col-xs-9 text-left" style={{ padding: '0', height: '50px', fontSize: '16px' }}>
