f836ac8da903ebdf91c750a1b7a9dc3345e330ff
Bryan Mateer <bryanxmateer@gmail.com>
2018-06-18 16:12:10 -0400

    fix styling for message

--- client/src/components/services/ServiceMessage.js
+++ client/src/components/services/ServiceMessage.js
@@ -10,14 +10,14 @@ const ServiceMessage = ({ position, message }) => {
     content =  
       <div className="col-xs-12 text-left">
         <p style={{ marginBottom: '2px', opacity: 0.9 }}><em>{message.user.username}</em></p>
-        <p style={{ fontSize: '16px', display: 'inline-block', padding: '5px', borderRadius: '10px', border: '1px solid black', margin: '0' }}>{message.body}</p>
+        <p style={{ fontSize: '16px', display: 'inline-block', padding: '5px', borderRadius: '10px', border: '1px solid black', margin: '0', maxWidth: '50%', wordWrap: 'break-word' }}>{message.body}</p>
         <p style={{ marginTop: '2px', opacity: 0.9 }}>{timeAgo.format(new Date(message.createdAt))}</p>
       </div>
   } else {
     content =
       <div className="col-xs-12 text-right">
         <p style={{ marginBottom: '2px', opacity: 0.9 }}><em>You</em></p>
-        <p style={{ fontSize: '16px', display: 'inline-block', padding: '5px', borderRadius: '10px', margin: '0', backgroundColor: '#33cc33', color: 'white' }}>{message.body}</p>
+        <p style={{ fontSize: '16px', display: 'inline-block', padding: '5px', borderRadius: '10px', margin: '0', backgroundColor: '#33cc33', color: 'white', maxWidth: '50%', wordWrap: 'break-word', textAlign: 'left' }}>{message.body}</p>
         <p style={{ marginTop: '2px', opacity: 0.9 }}>{ message.createdAt ? timeAgo.format(new Date(message.createdAt)) : "just now" }</p>
       </div>
   }
