7cc93b391274065f7fbc94d8e401fc2389cf6f74
Bryan Mateer <bryanxmateer@gmail.com>
2018-06-19 20:09:48 -0400

    add key for stripe client id

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   igdbKey: process.env.IGDB_KEY,
+  stripeClientID: process.env.STRIPE_CLIENT_ID,
   stripePKey: process.env.STRIPE_PKEY,
   stripeSKey: process.env.STRIPE_SKEY
 };
