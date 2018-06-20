ecd504745e3652f3f2cc45048560e1aaf6874aba
Bryan Mateer <bryanxmateer@gmail.com>
2018-06-19 20:10:58 -0400

    add route for connecting stripe account

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
+const keys = require('../config/keys')
 
 const uniqueChecker = require('../services/uniqueErrorChecker');
 const requireLogin = require('../middlewares/requireLogin');
@@ -21,4 +23,23 @@ module.exports = app => {
       return res.status(500).send('Unknown Server Error');
     }
   });
+
+  app.get('/api/stripe/connect', requireLogin, (req, res) => {
+    axios.post('https://connect.stripe.com/oauth/token', {
+      grant_type: 'authorization_code',
+      client_id: keys.stripeClientID,
+      client_secret: keys.stripeSKey,
+      code: req.query.code
+    })
+    .then(async response => {
+      const user = await User.findById(req.user._id);
+      user.stripeID = response.data.stripe_user_id
+      await user.save()
+      res.redirect('/dashboard')
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).send('Unknown Server Error')
+    })
+  })
 };
